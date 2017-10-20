a81bd562f3a6acc3d781fa190ee9a6933279ebf1
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-20 20:39:01 +0200

    Finished construction for a new contact

--- addressbook.js
+++ addressbook.js
@@ -1,25 +1,28 @@
 // button required to use
-function createContact(firstName) {
-  this.firstName = firstName;
-  //, lastName, phoneNumber, email
-  // this.lastName = lastName;
-  // this.phoneNumber = phoneNumber;
-  // this.email = email;
-};
+function createContact(firstName, lastName, phoneNumber, email) { 
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+}
 
 var sampleContact = {
   firstName: "Bill",
   lastName: "Door",
   phoneNumber: "01 294 323 434",
-  email: "billdoor@microsoft.com"
+  email: "billdoor@microsoft.com",
 }
+
+var contactList = [sampleContact];
+
 
 //onload sample contact
 function showSampleContact() {
+  document.getElementById('contactTitle').innerHTML = sampleContact.firstName + " " + sampleContact.lastName;    
   document.getElementById('firstName').innerHTML = sampleContact.firstName;
   document.getElementById('lastName').innerHTML = sampleContact.lastName;
   document.getElementById('phoneNumber').innerHTML = sampleContact.phoneNumber;
   document.getElementById('email').innerHTML = sampleContact.email;
 }
 
-document.write(sampleContact.firstName)
+contactList[contactList.length] = new createContact("John","Towel","40321241","johntowel@mail.com");
