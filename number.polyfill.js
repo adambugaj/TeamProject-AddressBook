3c799900ab45250e636020cbe4a0b52c9c5ed2e7
BinarForge <contact@binarforge.com>
2017-10-22 13:41:41 +0100

    Added a simple js polyfill extending Number type and example usage in index.

---
+++ number.polyfill.js
@@ -0,0 +1,16 @@
+/*
+Usage:
+    var foo = 34
+    foo.isPositive() returns true
+
+    var bar = -223
+    foo.isPositive() returns false
+*/
+
+/* First, check if functionality already exists */
+if(typeof Number.prototype.isPositive === 'undefined')
+{
+    /* Extend the Number's type funcionality using standard syntax
+       to make sure it will be available across all the browsers */
+    Number.prototype.isPositive = function(){ return this.valueOf() > 0 }
+}
