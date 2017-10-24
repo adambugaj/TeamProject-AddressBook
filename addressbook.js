ec1d91d5e6fbe243e67faa39b861160bdde820bd
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-24 19:14:34 +0200

    As u user, I can save the new contact details by submitting the form

--- addressbook.js
+++ addressbook.js
@@ -27,9 +27,22 @@ var contactList = [createContact(
 
 //onload sample contact
 function showContact(contactObj) {
-  document.getElementById('contactTitle').innerHTML = contactObj.getFullName();    
+  document.getElementById('contactTitle').innerHTML = contactObj.getFullName();
   document.getElementById('firstName').innerHTML = contactObj.firstName;
   document.getElementById('lastName').innerHTML = contactObj.lastName;
   document.getElementById('phoneNumber').innerHTML = contactObj.phoneNumber;
   document.getElementById('email').innerHTML = contactObj.email;
 }
+
+//click a button to create a new contact and show it in the list
+document.getElementById("createNewContact").addEventListener('click', function(e){
+    var inputFirstName = document.getElementById("inputFirstName").value;
+    var inputLastName = document.getElementById("inputLastName").value;
+    var inputPhoneNumber = document.getElementById("inputPhoneNumber").value;
+    var inputEmail = document.getElementById("inputEmail").value;
+
+    var newContact = new createContact(inputFirstName, inputLastName, inputPhoneNumber, inputEmail);
+    
+    e.preventDefault();
+});
+
