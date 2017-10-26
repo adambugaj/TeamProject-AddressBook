1521e05574725e7cdc4ee7f3f5e81bb39bf9ec36
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-26 18:18:45 +0200

    two cases to be done

--- addressbook.js
+++ addressbook.js
@@ -1,5 +1,7 @@
+// turn off the list of a new contact
+document.getElementById("contactList").style.display = "none";
 
-function createContact(firstName, lastName, phoneNumber, email) { 
+function createContact(firstName, lastName, phoneNumber, email) {
     return{
         firstName: firstName,
         lastName: lastName,
@@ -19,17 +21,69 @@ var sampleContact = {
 }
 
 var contactList = [createContact(
-    sampleContact.firstName, 
+    sampleContact.firstName,
     sampleContact.lastName,
-    sampleContact.phoneNumber, 
+    sampleContact.phoneNumber,
     sampleContact.email
 )];
 
-//onload sample contact
 function showContact(contactObj) {
-  document.getElementById('contactTitle').innerHTML = contactObj.getFullName();    
+  document.getElementById('contactTitle').innerHTML = contactObj.getFullName();
   document.getElementById('firstName').innerHTML = contactObj.firstName;
   document.getElementById('lastName').innerHTML = contactObj.lastName;
   document.getElementById('phoneNumber').innerHTML = contactObj.phoneNumber;
   document.getElementById('email').innerHTML = contactObj.email;
 }
+
+
+//show the list of contacts
+function showList(contactObj) {
+  document.getElementById('contactList').innerHTML += '<hr><h4 id="contactTitle">'+contactObj.getFullName()+'</h4>';
+
+  document.getElementById('contactList').innerHTML += '<li class="list-group-item">First Name:'+'<h5><span class="badge badge-secondary" id="firstName">'+contactObj.firstName+'</span></h5></li>';
+
+  document.getElementById('contactList').innerHTML += '<li class="list-group-item">Last Name:'+'<h5><span class="badge badge-secondary" id="lastName">'+contactObj.lastName+'</span></h5></li>';
+
+  document.getElementById('contactList').innerHTML += '<li class="list-group-item">Phone Number:'+'<h5><span class="badge badge-secondary" id="phoneNumber">'+contactObj.phoneNumber+'</span></h5></li>';
+
+  document.getElementById('contactList').innerHTML += '<li class="list-group-item">Email:'+'<h5><span class="badge badge-secondary" id="email">'+contactObj.email+'</span></h5></li>';
+
+}
+
+//click a button to create a new contact and show it in the list
+document.getElementById("createNewContact").addEventListener('click', function(e){
+    var inputFirstName = document.getElementById("inputFirstName").value;
+    var inputLastName = document.getElementById("inputLastName").value;
+    var inputPhoneNumber = document.getElementById("inputPhoneNumber").value;
+    var inputEmail = document.getElementById("inputEmail").value;
+
+//create a contact
+    var newContact = new createContact(inputFirstName, inputLastName, inputPhoneNumber, inputEmail);
+
+//save the contact
+    contactList.push(newContact);
+
+//show the contact
+    document.getElementById("contactList").style.display = "block";
+    showContact(newContact);
+
+    e.preventDefault();
+});
+
+
+//click to show the list of saved contacts
+document.getElementById('showContactList').addEventListener('click', function(){
+
+    //close the form of Sample Contact
+    document.getElementById("contactList").style.display = "block";
+    // Looping through the object of contacts
+    contactList.forEach(function(index){
+    showList(index);
+
+    });
+
+});
+
+
+// To do: remove sample contact when contacts button clicked
+// when clicking button submit, contact lists turns off.
