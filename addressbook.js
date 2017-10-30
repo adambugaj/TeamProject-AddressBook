6ca373cdb41550c581ce4b32bae33e2a5db941a2
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-30 19:15:07 +0100

    1009

--- addressbook.js
+++ addressbook.js
@@ -81,7 +81,7 @@ document.getElementById("createNewContact").addEventListener('click', function(e
 });
 
 
-//click to show the list of saved contact list
+//click to show the list of saved contact
 document.getElementById('showContactList').addEventListener('click', function(){
     
     //close the form of Sample Contact
