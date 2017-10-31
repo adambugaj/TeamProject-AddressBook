856f6299d9b668483c6f28a1aa5bf39ca834d659
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-31 16:35:10 +0100

    #1008 As a user, I can type only number to a phone-number-form

--- addressbook.js
+++ addressbook.js
@@ -1,7 +1,7 @@
 // turn off the list of a new contact
 document.getElementById("contactList").style.display = "none";
 
-function createContact(firstName, lastName, phoneNumber, email) {
+function createContact(firstName, lastName, phoneNumber, email) { 
     return{
         firstName: firstName,
         lastName: lastName,
@@ -21,16 +21,16 @@ var sampleContact = {
 }
 
 var contactList = [createContact(
-    sampleContact.firstName,
+    sampleContact.firstName, 
     sampleContact.lastName,
-    sampleContact.phoneNumber,
+    sampleContact.phoneNumber, 
     sampleContact.email
 )];
 
 function showContact(contactObj) {
-  document.getElementById('contactTitle').innerHTML = contactObj.getFullName();
-  document.getElementById('firstName').innerHTML = contactObj.firstName;
-  document.getElementById('lastName').innerHTML = contactObj.lastName;
+  document.getElementById('contactTitle').innerHTML = contactObj.getFullName();    
+  document.getElementById('firstName').innerHTML = contactObj.firstName;  
+  document.getElementById('lastName').innerHTML = contactObj.lastName;    
   document.getElementById('phoneNumber').innerHTML = contactObj.phoneNumber;
   document.getElementById('email').innerHTML = contactObj.email;
 }
@@ -38,16 +38,16 @@ function showContact(contactObj) {
 
 //show the list of contacts
 function showList(contactObj) {
-  document.getElementById('contactList').innerHTML += '<hr><h4 id="contactTitle">'+contactObj.getFullName()+'</h4>';
-
+  document.getElementById('contactList').innerHTML += '<hr><h4 id="contactTitle">'+contactObj.getFullName()+'</h4>';  
+    
   document.getElementById('contactList').innerHTML += '<li class="list-group-item">First Name:'+'<h5><span class="badge badge-secondary" id="firstName">'+contactObj.firstName+'</span></h5></li>';
-
+    
   document.getElementById('contactList').innerHTML += '<li class="list-group-item">Last Name:'+'<h5><span class="badge badge-secondary" id="lastName">'+contactObj.lastName+'</span></h5></li>';
-
+    
   document.getElementById('contactList').innerHTML += '<li class="list-group-item">Phone Number:'+'<h5><span class="badge badge-secondary" id="phoneNumber">'+contactObj.phoneNumber+'</span></h5></li>';
 
   document.getElementById('contactList').innerHTML += '<li class="list-group-item">Email:'+'<h5><span class="badge badge-secondary" id="email">'+contactObj.email+'</span></h5></li>';
-
+    
 }
 
 //click a button to create a new contact and show it in the list
@@ -56,32 +56,49 @@ document.getElementById("createNewContact").addEventListener('click', function(e
     var inputLastName = document.getElementById("inputLastName").value;
     var inputPhoneNumber = document.getElementById("inputPhoneNumber").value;
     var inputEmail = document.getElementById("inputEmail").value;
-
+    var phoneNumberString = Number(inputPhoneNumber);
 //create a contact
     var newContact = new createContact(inputFirstName, inputLastName, inputPhoneNumber, inputEmail);
+    
+    console.log(phoneNumberString);
+    
+//save the contact but first fill all blank spaces
+    if (inputFirstName === "") {
+        alert("Please type first name");
+    } else if (inputLastName === "") {
+        alert("Please type last name");
+    } else if (inputPhoneNumber === "") {
+        alert("Please type phone number");
+    }
+        // alert if user provided not a number
+      else if (phoneNumberString !== Number(inputPhoneNumber)) {
+          alert("Not number");
 
-//save the contact
-    contactList.push(newContact);
-
-//show the contact
-    document.getElementById("contactList").style.display = "block";
-    showContact(newContact);
-
+    } else if (inputEmail === "") {
+        alert("Please type email");
+    }
+     else {
+        contactList.push(newContact);
+        //show the contact
+        document.getElementById("contactList").style.display = "block";
+        showContact(newContact);
+    }
+    
     e.preventDefault();
 });
 
 
 //click to show the list of saved contacts
 document.getElementById('showContactList').addEventListener('click', function(){
-
+    
     //close the form of Sample Contact
     document.getElementById("contactList").style.display = "block";
     // Looping through the object of contacts
     contactList.forEach(function(index){
     showList(index);
-
+        
     });
-
+    
 });
 
 
